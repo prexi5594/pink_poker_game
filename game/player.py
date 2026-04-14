64bedeec9a038b93a9e6f3035f0660e7bd461b7d
Prexidazie Morara <moraraprexidazie@gmail.com>
2026-04-14 10:56:02 +0300

    PP-7 working on player class

--- game/player.py
+++ game/player.py
@@ -1,4 +1,12 @@
 import random
 import time
 
+class Player():
 
+    def __init__(self,type="pc",cards=[],bet=0,name="",amount=0):
+
+        self.name=name
+        self.type=type
+        self.cards=cards
+        self._bet=bet
+        self.amount=amount
