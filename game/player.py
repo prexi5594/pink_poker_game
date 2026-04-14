4375ab46676d2eb70ca75fcd5a5f0adc14c9f463
Prexidazie Morara <moraraprexidazie@gmail.com>
2026-04-14 10:57:36 +0300

    PP-7 working on player class

--- game/player.py
+++ game/player.py
@@ -10,3 +10,24 @@ class Player():
         self.cards=cards
         self._bet=bet
         self.amount=amount
+
+def place_initial_bet(self):
+        
+        while True:
+            amount=input(f"Place initial bet amount.Current amount is {self.amount}: ")
+
+            if amount.isdigit():
+                n=int(amount)
+                if n>0 and n<=self.amount:
+                    #self.amount=self.amount-n #use a setter 
+                    self.bet=n
+                    return n
+                
+                print("Invalid amount entered.")
+                print(f"amount must range from 1 to {self.amount}")
+                print("try again")
+            
+            else:
+                print(f"enter a number as valid amount between 1 and {self.amount}")
+
+   
