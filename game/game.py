c9f3fb7878bd2b4e7021bd2672e9824b93a0e7cb
Prexidazie Morara <moraraprexidazie@gmail.com>
2026-04-14 10:45:28 +0300

    PP-6   completed game class

--- game/game.py
+++ game/game.py
@@ -36,3 +36,28 @@ class Game():
         
         else:
             raise ValueError("The turn must be assined to a player object")
+
+
+
+if __name__ == "__main__":
+    game = Game()
+
+    print("\n  Starting Poker Game...\n")
+
+    print("=== FULL DECK ===")
+    game.deck.print_deck()
+
+    def show_pc_cards(player):
+        print("\n=== PC CARDS ===")
+        print("Card 1:")
+        player.cards[0].print_card()
+        print("Card 2: [Hidden] ")
+
+    def show_human_cards(player):
+        print("\n=== YOUR CARDS ===")
+        for i, card in enumerate(player.cards[:2], start=1):
+            print(f"Card {i}:")
+            card.print_card()
+
+    show_pc_cards(game.pc)
+    show_human_cards(game.human)
